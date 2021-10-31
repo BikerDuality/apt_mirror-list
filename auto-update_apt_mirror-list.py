8c15c556c7ff82b6d981a0af7a9639f515aaaad3
BikerDuality <22326595+BikerDuality@users.noreply.github.com>
2021-10-31 10:51:06 +0800

    mirror with more info

--- auto-update_apt_mirror-list.py
+++ auto-update_apt_mirror-list.py
@@ -5,44 +5,116 @@ debian_mirror_base_url='https://www.debian.org/mirror'
 class debain_mirror_parser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
-        self.mirror_list=set()
-        self.language_list=set()
+        self.mirror_list=[]
+        self.language_list=[]
+        self.lastlink=-1
+        self.mirror_info=''
     def handle_starttag(self, tag, attrs):
+        self.tag=tag
         if tag=='a':
-            if attrs[0][0]=='href' and attrs[0][1].startswith('list.'):
-                self.language_list.add(attrs[0][1])
+            if attrs[0][0]=='href' and attrs[0][1].startswith('list.') and attrs[0][1] not in self.language_list:
+                self.language_list.append(attrs[0][1])
             if attrs[0][0]=='rel' and attrs[0][1]=='nofollow':
-                self.mirror_list.add(attrs[1][1])
+                self.mirror_info=attrs[1][1]
+                self.lastlink=0
+    def handle_data(self, data: str):
+        if self.lastlink>=0:
+            if self.lastlink==2:
+                self.mirror_info+='\tarch:'+' arch:'.join(data.split(' '))
+            self.lastlink+=1
+    def handle_endtag(self, tag: str):
+        if tag=='tr':
+            self.lastlink=-1
+            if self.mirror_info and self.mirror_info not in self.mirror_list:
+                self.mirror_list.append(self.mirror_info)
+            self.mirror_info=''
 
 debain_request=requests.get(debian_mirror_base_url+'/list')
 debain_parser=debain_mirror_parser()
 debain_parser.feed(debain_request.text)
 language_index=0
 while language_index<len(debain_parser.language_list):
+    print('\rdebain',language_index+1,'/',len(debain_parser.language_list),list(debain_parser.language_list)[language_index],end='')
     debain_request=requests.get(debian_mirror_base_url+'/'+list(debain_parser.language_list)[language_index])
     debain_parser.feed(debain_request.text)
     language_index+=1
 with open('debian/mirrors.txt','w') as debain_mirror_file:
-        debain_mirror_file.write('\n'.join(debain_parser.mirror_list))
+    print('\rdebain',len(debain_parser.language_list),'/',len(debain_parser.language_list),'done')
+    debain_mirror_file.write('\n'.join(debain_parser.mirror_list))
 
-ubuntu_mirror_url='https://launchpad.net/ubuntu/+archivemirrors'
-class ubuntu_mirror_parser(HTMLParser):
+ubuntu_mirror_base_url='https://launchpad.net'
+class ubuntu_mirror_list_parser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
-        self.mirror_list=set()
-        self.href=''
+        self.mirror_list=[]
+        self.td_count=0
     def handle_starttag(self, tag, attrs):
-        if tag=='a':
+        if tag=='td':
+            self.td_count+=1
+        if tag=='a' and self.td_count==1:
+            if attrs[0][0]=='href':
+                self.mirror_list.append(attrs[0][1])
+    def handle_endtag(self, tag: str):
+        if tag=='tr':
+            self.td_count=0
+class ubuntu_mirror_info_parser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.tag=''
+        self.href=''
+        self.value=''
+        self.pre=0
+        self.td_count=0
+        self.architecture=''
+        self.architecture_list=[]
+        self.mirror_info='\t'
+    def handle_starttag(self, tag, attrs):
+        self.tag=tag
+        if tag=='pre':
+            self.pre=1
+        if tag=='a' and self.pre==1:
             if attrs[0][0]=='href':
                 self.href=attrs[0][1]
+        if tag=='option' and attrs[0][0]=='value':
+            self.value=attrs[0][1]
+        if tag=='option':
+            for attr in attrs:
+                if attr[0]=='value':
+                    self.value=attr[1]
+        if tag=='td':
+            self.td_count+=1
     def handle_data(self, data: str):
-        if self.href!='':
-            if self.href.startswith(data):
-                self.mirror_list.add(self.href)
+        if self.pre and 'deb' in data:
+            if data.strip()=='deb':
+                self.mirror_info+='type:deb '
+            else:
+                self.mirror_info+='type:deb-src '
+        if self.tag=='option' and data.startswith(self.value[0].upper()+self.value[1:]):
+            self.mirror_info+='codename:'+self.value+' '
+        if self.tag=='td':
+            if data.strip()=='\n':
+                self.td_count-=1
+            if self.td_count==2 and self.architecture=='':
+                self.architecture=data
+            if self.td_count==3 and data=='Up to date' and self.architecture not in self.architecture_list:
+                self.architecture_list.append(self.architecture)
+                self.mirror_info+='arch:'+self.architecture+' '
     def handle_endtag(self, tag: str):
-        self.href=''
-ubuntu_request=requests.get(ubuntu_mirror_url)
-ubuntu_parser=ubuntu_mirror_parser()
-ubuntu_parser.feed(ubuntu_request.text)
+        if tag=='pre':
+            self.pre=0
+        if tag=='tr':
+            self.td_count=0
+            self.architecture=''
+ubuntu_request=requests.get(ubuntu_mirror_base_url+'/ubuntu/+archivemirrors')
+ubuntu_list_parser=ubuntu_mirror_list_parser()
+ubuntu_list_parser.feed(ubuntu_request.text)
+ubuntu_mirror_list=[]
+for url in ubuntu_list_parser.mirror_list:
+    print('\rubuntu',len(ubuntu_mirror_list)+1,'/',len(ubuntu_list_parser.mirror_list),url,end='')
+    ubuntu_request=requests.get(ubuntu_mirror_base_url+url)
+    ubuntu_info_parser=ubuntu_mirror_info_parser()
+    ubuntu_info_parser.feed(ubuntu_request.text)
+    ubuntu_mirror_list.append(ubuntu_info_parser.href+ubuntu_info_parser.mirror_info[:-1])
 with open('ubuntu/mirrors.txt','w') as ubuntu_mirror_file:
-    ubuntu_mirror_file.write('\n'.join(ubuntu_parser.mirror_list))
+    print('\rubuntu',len(ubuntu_list_parser.mirror_list),'/',len(ubuntu_list_parser.mirror_list),'done')
+    ubuntu_mirror_file.write('\n'.join(ubuntu_mirror_list))
